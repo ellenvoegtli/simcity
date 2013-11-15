e5e3ccf06027bfbb32f74a200164511898407926
Marcus Eng <marcuste@usc.edu>
2013-11-14 19:22:59 -0800

    working on personagent

--- src/mainCity/PersonAgent.java
+++ src/mainCity/PersonAgent.java
@@ -1,10 +1,39 @@
 package mainCity;
 import agent.Agent;
+import role.Role;
+
+import java.util.*;
+import java.util.concurrent.Semaphore;
 
 public class PersonAgent extends Agent {
+	private enum PersonState {normal, working, inBuilding}
+	private enum PersonEvent {none, arrivedAtHome, arrivedAtWork, arrivedAtMarket, arrivedAtBank, timeToWork, needFood, gotFood, gotHungry, decidedRestaurant, needToBank}
+	private enum CityLocation {home, restaurant_david, restaurant_ellen, restaurant_ena, restaurant_jefferson, restaurant_marcus, bank, market}
+	
+	private int cash;
+	private boolean traveling;
+	private boolean onBreak;
+	private PersonState state;
+	private PersonEvent event;
+	private CityLocation destination;
+	private Semaphore isMoving = new Semaphore(0, true);
+	private List<Role> roles;
+	private PriorityQueue<Action> actions;
+	
+	PersonAgent() {
+		super();
+		
+		traveling = false;
+		onBreak = false;
+		state = PersonState.normal;//maybe different if we start everyone in home
+		event = PersonEvent.none;
+		destination = CityLocation.home;
+		roles = Collections.synchronizedList(new ArrayList<Role>());
+	}
 	
 	protected boolean pickAndExecuteAnAction() {
-		// TODO Auto-generated method stub
+		
+		
 		return false;
 	}
 
@@ -12,4 +41,12 @@ public class PersonAgent extends Agent {
 		this.stateChanged();
 	}
 	
+	
+	private enum ActionState {created, inProgress, Done}
+	private enum ActionType {work, restaurant, market, bankWithdraw, bankDeposit, bankLoan, home}
+	class Action {
+		ActionState state;
+		ActionType type;
+		int priority;
+	}
 }
