80c11f55dc7983fae317ceab3048dab82c99fe1a
Ellen Voegtli <voegtli@usc.edu>
2013-11-19 20:39:35 -0800

    updated MainCashier's hereIsChange

--- src/mainCity/interfaces/MainCashier.java
+++ src/mainCity/interfaces/MainCashier.java
@@ -4,5 +4,5 @@ import java.util.Map;
 public interface MainCashier {
 	public abstract void msgHereIsMarketBill(Map<String, Integer> inventory, double billAmount, String deliveryPerson);
 	
-	public abstract void msgHereIsChange(double amount);
+	public abstract void msgHereIsChange(double amount, String deliveryPerson);
 }
