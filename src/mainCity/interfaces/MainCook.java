68db22c28112180fe576d6cac2f0d9ecc308f96a
Ena Trokic <trokic@usc.edu>
2013-11-21 00:43:15 -0800

    mainmarket changes

--- src/mainCity/interfaces/MainCook.java
+++ src/mainCity/interfaces/MainCook.java
@@ -2,7 +2,9 @@ package mainCity.interfaces;
 import java.util.Map;
 import mainCity.market.*;
 
-public interface MainCook {
+public interface MainCook 
+{
 	public abstract void setMarketGreeter(MarketGreeterRole g);
 	public abstract void msgHereIsYourOrder(Map<String, Integer> inventory);
+	
 }
