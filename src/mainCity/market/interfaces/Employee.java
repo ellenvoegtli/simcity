0853ce2b85c0bd1b9a771b46098a91a4b2b73554
Ellen Voegtli <voegtli@usc.edu>
2013-11-25 18:09:25 -0800

    all interface/mock files for market have the proper stubs

--- src/mainCity/market/interfaces/Employee.java
+++ src/mainCity/market/interfaces/Employee.java
@@ -1,11 +1,26 @@
 package mainCity.market.interfaces;
 
+import java.util.Map;
+
+import mainCity.interfaces.MainCashier;
+import mainCity.interfaces.MainCook;
+import mainCity.market.MarketCustomerRole;
+import mainCity.market.MarketEmployeeRole.MyBusiness;
+import mainCity.market.MarketEmployeeRole.MyCustomer;
+
 
 public interface Employee {
 	//public abstract void 
 	//all MESSAGE method stubs
 	
 	
-	
+    public abstract void msgAssignedToBusiness(String restaurantName, MainCook cook, MainCashier cashier, Map<String, Integer>inventory);
+	public abstract void msgAssignedToCustomer(MarketCustomerRole c, int waitPosX, int waitPosY);
+	public abstract void msgHereIsMyOrder(MarketCustomerRole c, Map<String, Integer> inventory, String deliveryMethod);
+	public abstract void msgHereIsBill(MarketCustomerRole c, double amount);		//from cashier
+	public abstract void msgHereIsBill(String name, double amount);		//from cashier
+	public abstract void msgOrderFulfilled(MyCustomer mc);		//from timer
+	public abstract void msgOrderFulfilled(MyBusiness mb);		//from timer
+	public abstract void msgDoneAndLeaving(MarketCustomerRole c);
 	
 }
--- src/mainCity/market/test/mock/MockEmployee.java
+++ src/mainCity/market/test/mock/MockEmployee.java
@@ -1,9 +1,42 @@
 package mainCity.market.test.mock;
 
+import java.util.Map;
+
+import mainCity.interfaces.MainCashier;
+import mainCity.interfaces.MainCook;
+import mainCity.market.MarketCustomerRole;
+import mainCity.market.MarketEmployeeRole.MyBusiness;
+import mainCity.market.MarketEmployeeRole.MyCustomer;
+import mainCity.market.interfaces.Employee;
+
 public class MockEmployee extends Mock implements Employee {
 	MockEmployee(String name){
 		super(name);
 	}
 	
+	public void msgAssignedToBusiness(String restaurantName, MainCook cook, MainCashier cashier, Map<String, Integer>inventory){
+		
+	}
+	public void msgAssignedToCustomer(MarketCustomerRole c, int waitPosX, int waitPosY){
+		
+	}
+	public void msgHereIsMyOrder(MarketCustomerRole c, Map<String, Integer> inventory, String deliveryMethod){
+		
+	}
+	public void msgHereIsBill(MarketCustomerRole c, double amount){		//from cashier
+		
+	}
+	public void msgHereIsBill(String name, double amount){		//from cashier
+		
+	}
+	public void msgOrderFulfilled(MyCustomer mc){		//from timer
+		
+	}
+	public void msgOrderFulfilled(MyBusiness mb){		//from timer
+		
+	}
+	public void msgDoneAndLeaving(MarketCustomerRole c){
+		
+	}
 	
 }
