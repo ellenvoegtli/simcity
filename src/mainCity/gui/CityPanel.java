331a24e90c84169d1b54d8b6918489c4b0af562c
Marcus Eng <marcuste@usc.edu>
2013-12-09 14:56:09 -0800

    forgot this. close #167

--- src/mainCity/gui/CityPanel.java
+++ src/mainCity/gui/CityPanel.java
@@ -78,7 +78,7 @@ public class CityPanel extends JPanel{
 		 };
 
 		 ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-		 executor.scheduleAtFixedRate(cityClock, 0, 2, TimeUnit.SECONDS); //Timer goes off every 15 seconds
+		 executor.scheduleAtFixedRate(cityClock, 0, 15, TimeUnit.SECONDS); //Timer goes off every 15 seconds
 	}
 	
 	public void addDeliveryGui(MarketDeliveryManRole d, int x, int y){
